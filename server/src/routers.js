58c90ee593ce614f3c30795ad2709f610a7cad07
ThiagoFazzi <thiago@xtwo.com.br>
2019-08-29 20:37:35 +0200

    added check to empty result, also transform a result in a unique array of characters url and return promise all for each url

--- server/src/routers.js
+++ server/src/routers.js
@@ -2,7 +2,23 @@ import Axios from 'axios'
 
 export const getCharacterByFilm = (req, res) => {
   Axios.get(`https://swapi.co/api/films/?search=${req.params.term}`)
-    .then(films => {})
+    .then(films => {
+      if (films.data.count === 0) {
+        new Error('Film not found, try again!!')
+      }
+
+      let charactersArray = []
+
+      films.data.results.forEach(characters => {
+        characters.characters.forEach(character => {
+          charactersArray.push(character)
+        })
+      })
+
+      return Promise.all(
+        checkDuplicate(charactersArray).map(url => Axios.get(url))
+      )
+    })
     .then(characters => {})
     .catch(console.error)
 }
