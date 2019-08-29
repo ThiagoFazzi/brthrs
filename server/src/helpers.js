eb629cff8280216f0faa76ebffdd96c45b026609
ThiagoFazzi <thiago@xtwo.com.br>
2019-08-29 21:03:36 +0200

    get sortByHeight helper funcion also get promises value as characters details, sort for character height and return result do the client

--- server/src/helpers.js
+++ server/src/helpers.js
@@ -3,3 +3,11 @@ export const checkDuplicate = array => {
     return array.indexOf(item) >= index
   })
 }
+
+export const sortByHeight = (array, sort) => {
+  if (sort === 'DESC') {
+    return array.sort((a, b) => b.height - a.height)
+  } else {
+    return array.sort((a, b) => a.height - b.height)
+  }
+}
--- server/src/routers.js
+++ server/src/routers.js
@@ -20,6 +20,27 @@ export const getCharacterByFilm = (req, res) => {
         checkDuplicate(charactersArray).map(url => Axios.get(url))
       )
     })
-    .then(characters => {})
+    .then(characters => {
+      const charactersObject = characters.map(character => {
+        return {
+          name: character.data.name,
+          height: character.data.height,
+          url: character.data.url,
+          skinColor: character.data.skin_color,
+          mass: character.data.mass,
+          gender: character.data.gender,
+          hairColor: character.data.hair_color,
+          eyeColor: character.data.eye_color,
+          birthYear: character.data.birth_year
+        }
+      })
+
+      const charactersObjectSorted = sortByHeight(
+        charactersObject,
+        req.params.sort
+      )
+
+      res.send(charactersObjectSorted)
+    })
     .catch(console.error)
 }
