3e5fcc304f6ab304a9fbc1617138e592829baa21
ThiagoFazzi <thiago@xtwo.com.br>
2019-08-30 10:24:14 +0200

    added List components, also import into Layout and give the list of characters as props to List component

---
+++ client/src/components/List.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="list" v-if="!list">
+    <ul></ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "List",
+  props: ["list"]
+};
+</script>
+
+<style>
+.list {
+  width: 90vw;
+  margin: 0 auto;
+  align-items: center;
+}
+
+.list ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+</style>
